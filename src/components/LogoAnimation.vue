058a40ab1f05e928f99bee86c70ae8fed51fae46
bgfxc4 <bindermarc@gmx.de>
2022-07-23 15:44:21 +0200

    experiment with path generation

--- src/components/LogoAnimation.vue
+++ src/components/LogoAnimation.vue
@@ -2,7 +2,7 @@
 	<div id="LogoAnimation">
 		<div id="logo-container">
 			<h1>bgfxc4</h1>
-			<img src="/logo.svg" id="logo">
+			<img @load="logoLoaded = true; generateAntPaths()" src="/logo.svg" id="logo">
 		</div>
 
 		<svg id="paths">
@@ -27,42 +27,88 @@ export default {
 		NavigationManager,
 		ToggleSwitch
 	},
+	data () {
+		return {
+			logoLoaded: false
+		}
+	},
 	methods: {
 		generateAntPaths () {
+			if (!this.logoLoaded) return
+			console.log("\n\n\n\nasdasd")
 			if (this.$route.name != "Home") return
 
 			$(".path").remove()
-			for (var i = 0; i < 6; i++) {
+			for (var i = 0; i < 1; i++) {
 				var el = this.generatePath(`ant-${i}`)
 				$("#paths")[0].innerHTML += el
 			}
+
 			$(".path").one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", () => console.log("finished anim"))
 		},
 		generatePath (id) {
-			var w = window.innerWidth
-			var h = window.innerHeight
+			var w = $("#paths").width()
+			var h = $("#paths").height()
+
+			var lW = $("#logo").width()
+			var lH = $("#logo").height()
+			var lOl = $("#logo").offset().left
+			var lOt = $("#logo").offset().top
 
 			var goVertical = Math.random() > .5
 			var startLeftOrTop = Math.random() > .5
 
-			var d = ""
+			var d = []
 
-			if (goVertical) {
-				d = `M${Math.floor(Math.random() * w)} ${startLeftOrTop ? 0 : h} `
-			} else {
-				d = `M${startLeftOrTop ? 0 : w} ${Math.floor(Math.random() * h)} `
+			let currentPos = {
+				x: null,
+				y: null
 			}
 
+			if (goVertical) {
+				currentPos.x = Math.floor(Math.random() * w)
+				currentPos.y = (startLeftOrTop ? 0 : h)
+				d = `M${currentPos.x} ${currentPos.y} `
+			} else {
+				currentPos.x = (startLeftOrTop ? 0 : h)
+				currentPos.y = Math.floor(Math.random() * h)
+				d = `M${currentPos.x} ${currentPos.y} `
+			}
+			debugger;
 			while (Math.random() > .15) {
 				if (goVertical) {
-					d += `V${Math.floor(Math.random() * h)} `
+					let move = null
+					if (currentPos.x > lOl && currentPos.x < (lOl + lW)) { // currentPos is above or beneath logo
+						console.log("asda")
+						if (currentPos.y < lOt) { // currentPos is above logo
+							move = Math.floor(Math.random() * lOt)
+						} else { // currentPos is beneath logo
+							move = Math.floor(lOt + Math.random() * ((h - lOt) - lH))
+						}
+					} else {
+						move = Math.floor(Math.random() * h)
+					}
+					d += `V${move} `
+					currentPos.y = move
 				} else {
-					d += `H${Math.floor(Math.random() * w)} `
+					let move = null
+					if (currentPos.y > lOt && currentPos.y < (lOt + lH)) { // currentPos is at height of logo
+						console.log("asd")
+						if (currentPos.x < lOl) { // currentPos is left of logo
+							move = Math.floor(Math.random() * lOl)
+						} else { // currentPos is right of logo
+							move = Math.floor(lOl + Math.random() * ((w - lOl) - lW))
+						}
+					} else {
+						move = Math.floor(Math.random() * h)
+					}
+					d += `H${move} `
+					currentPos.x = move
 				}
 				goVertical = !goVertical
 			}
 			d += goVertical ? `V${(Math.random() > .5) ? h : 0}` : `H${(Math.random() > .5) ? w : 0}`
-
+			console.log(d)
 			var p = `<path id="${id}" d="${d}" class="path" style="animation-delay: ${Math.floor(Math.random() * 5)}s; --animation-time: ${8 + Math.floor(Math.random() * 15)}s"/>`
 			return p
 		},
@@ -145,13 +191,14 @@ export default {
 
 	.path {
 		fill: none;
-		stroke: #0000;
-		stroke-dasharray: 6% 500%;
+		/*stroke: #0000;*/
+		stroke: black;
+		/*stroke-dasharray: 6% 500%;*/
 		stroke-width: 0.6%;
-		stroke-dashoffset: 0%;
+		/*stroke-dashoffset: 0%;*/
 		stroke-linecap: round;
 		stroke-linejoin: round;
-		animation: stroke-offset var(--animation-time) linear infinite
+		/*animation: stroke-offset var(--animation-time) linear infinite*/
 	}
 
 	@keyframes stroke-offset {
